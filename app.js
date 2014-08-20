e2dcbbf0ac9cdd05b5eb8de458b6188f44416ff1
Rohit Saxena <aerohit.saxena@gmail.com>
2014-08-20 17:35:51 +0200

    The first controller.

--- app.js
+++ app.js
@@ -1,3 +1,15 @@
 'use strict';
 
-var myApp = angular.module('store', []);
+(function(){
+  var app = angular.module('store', []);
+
+  app.controller('StoreController', function() {
+    this.product = gem;
+  });
+
+  var gem = {
+    name:          'Dodecahedron',
+    price:         2.95,
+    description:   '...',
+  };
+})();
