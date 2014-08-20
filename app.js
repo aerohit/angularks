5505d70f76de787a6f1ba0d8d86206f1f840f9f8
Rohit Saxena <aerohit.saxena@gmail.com>
2014-08-20 22:24:27 +0200

    Created a form controller to handle the logic of submitting forms.

--- app.js
+++ app.js
@@ -19,6 +19,15 @@
     };
   });
 
+  app.controller('ReviewController', function() {
+    this.review = {};
+
+    this.addReview = function(product) {
+      product.reviews.push(this.review);
+      this.review = {};
+    };
+  });
+
   var gems = [
     {
       name:          'Dodecahedron',
